bf62c588e765ad0f1cb85aee341b82293e931b58
UserNotFound-404 <sergijkogutka@gmail.com>
2023-03-29 09:46:35 +0300

    Creating models

--- carFinder/models.py
+++ carFinder/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Brand(models.Model):
+	name = models.CharField(max_length = 50)
+	country = models.CharField(max_length = 80)
+
+class Model(models.Model):
+	name = models.CharField(max_length = 80)
+	year = models.IntegerField()
+	body_style = models.CharField(max_length = 80)
+
+class Car(models.Model):
+	brand = models.ForeignKey(Brand, on_delete = models.CASCADE)
+	model = models.ForeignKey(Model, on_delete = models.CASCADE)
+	price = models.IntegerField()
+	mileage = models.IntegerField()
+	extr_color = models.CharField(max_length = 50)
+	intr_color = models.CharField(max_length = 50)
+	fuel_type = models.CharField(max_length = 50)
+	transmission = models.CharField(max_length = 50)
+	engine = models.CharField(max_length = 50)
+	on_sale = models.BooleanField()
+	
