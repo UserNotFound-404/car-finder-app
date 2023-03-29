ae8f9feab97ae0d75233fed2a0a6ee8e70ed9bbe
UserNotFound-404 <sergijkogutka@gmail.com>
2023-03-29 18:23:39 +0300

    Deleting unusable code

--- carFinder/views.py
+++ carFinder/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+from .models import *
+
 def index(request):
 	return render(request, "main/index.html")
-
-def about(request):
-	return HttpResponse("<h2>About page</h2>")
