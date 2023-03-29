c294873c7aa49f6c50aae470546fc535464d4d3a
UserNotFound-404 <sergijkogutka@gmail.com>
2023-03-29 10:16:12 +0300

    Adding forms to admin panel

--- carFinder/urls.py
+++ carFinder/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-
+from django.contrib import admin
 
 urlpatterns = [
 	path('', views.index, name='home'),
-	path('about/', views.about)
+	path('about/', views.about),
+	path('admin/', admin.site.urls),
 ]
