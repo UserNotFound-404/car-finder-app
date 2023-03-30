b7004f849e1c500d2a5ab7c9c65e6464e24e4056
UserNotFound-404 <sergijkogutka@gmail.com>
2023-03-30 16:57:28 +0300

    Adding login/registration forms

--- carFinder/urls.py
+++ carFinder/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
 from django.contrib import admin
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
 	path('', views.index, name='home'),
 	path('admin/', admin.site.urls),
+	path('user/create/', views.register_request, name='register'),
+	path('user/login/', views.login_request, name='login'),
+	path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
