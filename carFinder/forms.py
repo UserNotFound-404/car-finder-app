7bb5922a24aca89e1af42df4476719a3c76eb55b
UserNotFound-404 <sergijkogutka@gmail.com>
2023-03-31 03:23:00 +0300

    Creating filters base for each page

--- carFinder/forms.py
+++ carFinder/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import *
 
-
-# Create your forms here.
 
 class NewUserForm(UserCreationForm):
 	email = forms.EmailField(required=True)
@@ -18,3 +17,24 @@ class NewUserForm(UserCreationForm):
 		if commit:
 			user.save()
 		return user
+
+class ModelFilterForm(forms.Form):
+	model = forms.ModelChoiceField(queryset=Model.objects.values_list('name', flat=True).distinct(), required=False, empty_label="Select model name")
+	max_year = forms.IntegerField(required=False)
+	min_year = forms.IntegerField(required=False)
+	model_style = forms.ModelChoiceField(queryset=Model.objects.values_list('body_style', flat=True).distinct(), required=False, empty_label="Select body style")
+
+class BrandFilterForm(forms.Form):
+	brand = forms.ModelChoiceField(queryset=Brand.objects.values_list('name', flat=True).distinct(), empty_label="Select brand", required=False)
+	country = forms.ModelChoiceField(queryset=Brand.objects.values_list('country', flat=True).distinct(), empty_label="Select country", required=False)
+
+class CarFilterForm(ModelFilterForm, BrandFilterForm):
+	min_price = forms.IntegerField(required = False)
+	max_price = forms.IntegerField(required = False)
+	min_mileage = forms.IntegerField(required = False)
+	max_mileage = forms.IntegerField(required = False)
+	select_extr_color = forms.BooleanField(label="Choose exterier color", required=False)
+	extr_color = forms.CharField(widget=forms.TextInput(attrs={'type': 'color', 'value': ''}))
+	select_intr_color = forms.BooleanField(label="Choose interier color", required=False)
+	intr_color = forms.CharField(widget=forms.TextInput(attrs={'type': 'color', 'value': ''}))
+	fuel_type = forms.ModelChoiceField(queryset=Car.objects.values_list('fuel_type', flat=True).distinct(), empty_label="Select fuel type", required=False)
--- carFinder/views.py
+++ carFinder/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
-from .forms import NewUserForm
+from .forms import *
 from django.contrib.auth import login, authenticate
 from django.shortcuts import redirect
 from django.contrib import messages
@@ -14,17 +14,88 @@ def all_cars(request):
 	cars = Car.objects.all()
 	return render(request, "main/car_list.html", {'cars': cars})
 
+def get_model_filter(request):
+	model_name = request.GET.get('model')
+	max_year = request.GET.get('max_year')
+	min_year = request.GET.get('min_year')
+	model_style = request.GET.get('model_style')
+	return model_name, max_year, min_year, model_style
+
+def get_brand_filter(request):
+	brand_name = request.GET.get('brand')
+	brand_country = request.GET.get('country')
+	return brand_name, brand_country
+
 def car_list(request):
 	cars = Car.objects.filter(on_sale = True)
-	return render(request, "main/car_list.html", {'cars': cars})
+	filter_form = CarFilterForm(request.GET or None)
+
+	brand_name, brand_country = get_brand_filter(request)
+	model_name, max_year, min_year, model_style = get_model_filter(request)
+
+	min_price = request.GET.get('min_price')
+	max_price = request.GET.get('max_price')
+	min_mileage = request.GET.get('min_mileage')
+	max_mileage = request.GET.get('max_mileage')
+	extr_color = request.GET.get('extr_color')
+	intr_color = request.GET.get('intr_color')
+	fuel_type = request.GET.get('fuel_type')
+	selected_intr_color = request.GET.get('select_intr_color')
+	selected_extr_color = request.GET.get('select_extr_color')
+
+	if brand_name:
+		cars = cars.filter(brand__name=brand_name)
+	if brand_country:
+		cars = cars.filter(brand__country=brand_country)
+
+	if model_name:
+		cars = cars.filter(model__name=model_name)
+	if max_year:
+		cars = cars.filter(model__year__lte=max_year)
+	if min_year:
+		cars = cars.filter(model__year__gte=min_year)
+	if model_style:
+		cars = cars.filter(model__body_style=model_style)
+
+	if min_price:
+		cars = cars.filter(price__gte=min_price)
+	if max_price:
+		cars = cars.filter(price__lte=max_price)
+	if min_mileage:
+		cars = cars.filter(mileage__gte=min_mileage)
+	if max_mileage:
+		cars = cars.filter(mileage__lte=max_mileage)
+	if extr_color and selected_extr_color:
+		cars = cars.filter(extr_color=extr_color)
+	if intr_color and selected_intr_color:
+		cars = cars.filter(intr_color=intr_color)
+	if fuel_type:
+		cars = cars.filter(fuel_type=fuel_type)
+	return render(request, 'main/car_list.html', {'cars': cars, 'filter_form': filter_form})
 
 def brand_list(request):
 	brands = Brand.objects.all()
-	return render(request, "main/brand_list.html", {'brands': brands})
+	filter_form = BrandFilterForm(request.GET or None)
+	brand_name, brand_country = get_brand_filter(request)
+	if brand_name:
+		brands = brands.filter(name=brand_name)
+	if brand_country:
+		brands = brands.filter(country=brand_country)
+	return render(request, "main/brand_list.html", {'brands': brands, 'filter_form': filter_form})
 
 def model_list(request):
 	models = Model.objects.all()
-	return render(request, "main/model_list.html", {'models': models})
+	filter_form = ModelFilterForm(request.GET or None)
+	model_name, max_year, min_year, model_style = get_model_filter(request)
+	if model_name:
+		models = models.filter(name=model_name)
+	if max_year:
+		models = models.filter(year__lte=max_year)
+	if min_year:
+		models = models.filter(year__gte=min_year)
+	if model_style:
+		models = models.filter(body_style=model_style)
+	return render(request, "main/model_list.html", {'models': models, 'filter_form': filter_form})
 
 def register_request(request):
 	if request.method == "POST":
