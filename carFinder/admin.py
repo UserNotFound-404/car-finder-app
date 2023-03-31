c2519bb6d594ad46417f9a29d1dd3060c0c41d3d
UserNotFound-404 <sergijkogutka@gmail.com>
2023-03-31 15:44:10 +0300

    Edited color forms in admin panel to color type

--- carFinder/admin.py
+++ carFinder/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
 from .models import *
+from django import forms
+
+
+class CarForm(forms.ModelForm):
+	class Meta:
+		model = Car
+		fields = '__all__'
+		widgets = {
+				'extr_color': forms.TextInput(attrs={'type': 'color'}),
+				'intr_color': forms.TextInput(attrs={'type': 'color'}),
+		}
+
+class CarAdmin(admin.ModelAdmin):
+	form = CarForm
 
 admin.site.register(Brand)
 admin.site.register(Model)
-admin.site.register(Car)
+admin.site.register(Car, CarAdmin)
+
