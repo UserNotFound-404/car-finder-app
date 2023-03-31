d176c03a92c8dcaec87689cca379e3ca54a5348c
UserNotFound-404 <sergijkogutka@gmail.com>
2023-03-31 17:27:48 +0300

    Checking if valid form

--- carFinder/views.py
+++ carFinder/views.py
@@ -42,71 +42,73 @@ def car_list(request):
 	fuel_type = request.GET.get('fuel_type')
 	selected_intr_color = request.GET.get('select_intr_color')
 	selected_extr_color = request.GET.get('select_extr_color')
+	if filter_form.is_valid():
+		if brand_name:
+			cars = cars.filter(brand__name=brand_name)
+		if brand_country:
+			cars = cars.filter(brand__country=brand_country)
 
-	if brand_name:
-		cars = cars.filter(brand__name=brand_name)
-	if brand_country:
-		cars = cars.filter(brand__country=brand_country)
+		if model_name:
+			cars = cars.filter(model__name=model_name)
+		if max_year:
+			cars = cars.filter(model__year__lte=max_year)
+		if min_year:
+			cars = cars.filter(model__year__gte=min_year)
+		if model_style:
+			cars = cars.filter(model__body_style=model_style)
 
-	if model_name:
-		cars = cars.filter(model__name=model_name)
-	if max_year:
-		cars = cars.filter(model__year__lte=max_year)
-	if min_year:
-		cars = cars.filter(model__year__gte=min_year)
-	if model_style:
-		cars = cars.filter(model__body_style=model_style)
-
-	if min_price:
-		cars = cars.filter(price__gte=min_price)
-	if max_price:
-		cars = cars.filter(price__lte=max_price)
-	if min_mileage:
-		cars = cars.filter(mileage__gte=min_mileage)
-	if max_mileage:
-		cars = cars.filter(mileage__lte=max_mileage)
-	if extr_color and selected_extr_color:
-		temp_car_list = cars
-		extr_color_hsl = Car.get_hsl_color(extr_color)
-		for car in temp_car_list:
-			car_extr_hsl_color = Car.get_hsl_color(car.extr_color)
-			distance = Car.get_color_distance(car_extr_hsl_color, extr_color_hsl)
-			if distance > 0.21 and car in cars:
-				cars = cars.exclude(id=car.id)
-	if intr_color and selected_intr_color:
-		temp_car_list = cars
-		intr_color_hsl = Car.get_hsl_color(intr_color)
-		for car in temp_car_list:
-			car_intr_hsl_color = Car.get_hsl_color(car.intr_color)
-			distance = Car.get_color_distance(car_intr_hsl_color, intr_color_hsl)
-			if distance > 0.21 and car in cars:
-				cars = cars.exclude(id=car.id)
-	if fuel_type:
-		cars = cars.filter(fuel_type=fuel_type)
+		if min_price:
+			cars = cars.filter(price__gte=min_price)
+		if max_price:
+			cars = cars.filter(price__lte=max_price)
+		if min_mileage:
+			cars = cars.filter(mileage__gte=min_mileage)
+		if max_mileage:
+			cars = cars.filter(mileage__lte=max_mileage)
+		if extr_color and selected_extr_color:
+			temp_car_list = cars
+			extr_color_hsl = Car.get_hsl_color(extr_color)
+			for car in temp_car_list:
+				car_extr_hsl_color = Car.get_hsl_color(car.extr_color)
+				distance = Car.get_color_distance(car_extr_hsl_color, extr_color_hsl)
+				if distance > 0.21 and car in cars:
+					cars = cars.exclude(id=car.id)
+		if intr_color and selected_intr_color:
+			temp_car_list = cars
+			intr_color_hsl = Car.get_hsl_color(intr_color)
+			for car in temp_car_list:
+				car_intr_hsl_color = Car.get_hsl_color(car.intr_color)
+				distance = Car.get_color_distance(car_intr_hsl_color, intr_color_hsl)
+				if distance > 0.21 and car in cars:
+					cars = cars.exclude(id=car.id)
+		if fuel_type:
+			cars = cars.filter(fuel_type=fuel_type)
 	return render(request, 'main/car_list.html', {'cars': cars, 'filter_form': filter_form})
 
 def brand_list(request):
 	brands = Brand.objects.all()
 	filter_form = BrandFilterForm(request.GET or None)
 	brand_name, brand_country = get_brand_filter(request)
-	if brand_name:
-		brands = brands.filter(name=brand_name)
-	if brand_country:
-		brands = brands.filter(country=brand_country)
+	if filter_form.is_valid():
+		if brand_name:
+			brands = brands.filter(name=brand_name)
+		if brand_country:
+			brands = brands.filter(country=brand_country)
 	return render(request, "main/brand_list.html", {'brands': brands, 'filter_form': filter_form})
 
 def model_list(request):
 	models = Model.objects.all()
 	filter_form = ModelFilterForm(request.GET or None)
 	model_name, max_year, min_year, model_style = get_model_filter(request)
-	if model_name:
-		models = models.filter(name=model_name)
-	if max_year:
-		models = models.filter(year__lte=max_year)
-	if min_year:
-		models = models.filter(year__gte=min_year)
-	if model_style:
-		models = models.filter(body_style=model_style)
+	if filter_form.is_valid():
+		if model_name:
+			models = models.filter(name=model_name)
+		if max_year:
+			models = models.filter(year__lte=max_year)
+		if min_year:
+			models = models.filter(year__gte=min_year)
+		if model_style:
+			models = models.filter(body_style=model_style)
 	return render(request, "main/model_list.html", {'models': models, 'filter_form': filter_form})
 
 def register_request(request):
